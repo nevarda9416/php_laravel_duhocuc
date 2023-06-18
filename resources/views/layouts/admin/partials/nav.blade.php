ce0eb6670c88702921420b80fb6afbe9a0340cc0
tudao <nevarda9416@gmail.com>
2023-06-18 16:20:19 +0700

    - BE: widget các trang

--- resources/views/layouts/admin/partials/nav.blade.php
+++ resources/views/layouts/admin/partials/nav.blade.php
@@ -14,11 +14,45 @@
             <li><a href="{{ url('cms/banners') }}"><i class="fa fa fa-picture-o"></i>Quản lý banner</a></li>
             <li><a href="{{ url('cms/medias') }}"><i class="fa fa-file-o"></i>Quản lý file</a></li>
             <li><a href="{{ url('cms/contact') }}"><i class="fa fa-globe"></i>Quản lý thông tin website</a></li>
-            <li><a><i class="fa fa-laptop"></i>QL nội dung trang chủ<span class="fa fa-chevron-down"></span></a>
+            <li><a><i class="fa fa-laptop"></i>Nội dung trang chủ<span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
-                    <li><a href="{{ url('cms/widgets/homepage/top_banner') }}">Banner đầu trang</a></li>
-                    <li><a href="{{ url('cms/widgets/homepage/middle_banner') }}">Banner giữa trang</a></li>
-                    <li><a href="{{ url('cms/widgets/homepage/trend') }}">Xu hướng</a></li>
+                    <li><a href="{{ url('cms/widgets/homepage/criteria') }}">Đồng hành cùng bạn</a></li>
+                </ul>
+            </li>
+            <li><a style="font-size:12px"><i class="fa fa-laptop"></i>Nội dung trang du học Úc<span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="{{ url('cms/widgets/study/description') }}">Giới thiệu</a></li>
+                    <li><a href="{{ url('cms/widgets/study/why_chosen') }}">Tại sao nên chọn du học Úc</a></li>
+                    <li><a href="{{ url('cms/widgets/study/request_input') }}">Yêu cầu đầu vào du học Úc</a></li>
+                    <li><a href="{{ url('cms/widgets/study/middle_banner') }}">Banner giữa trang</a></li>
+                    <li><a href="{{ url('cms/widgets/study/cost') }}">Chi phí du học Úc</a></li>
+                    <li><a href="{{ url('cms/widgets/study/why_skypacific') }}">Tại sao nên chọn SKY PACIFIC</a></li>
+                    <li><a href="{{ url('cms/widgets/study/experience') }}">Trải nghiệm cựu học sinh</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-laptop"></i>Nội dung trang học bổng<span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="{{ url('cms/widgets/scholarship/right_banner') }}">Banner cột phải</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-laptop"></i>Nội dung trang hội thảo<span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="{{ url('cms/widgets/seminar/middle_banner') }}">Banner giữa trang</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-laptop"></i>Nội dung trang tư vấn<span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="{{ url('cms/widgets/consultation/right_banner') }}">Banner cột phải</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-laptop"></i>Nội dung trang tuyển dụng<span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="{{ url('cms/widgets/recruitment/right_banner') }}">Banner cột phải</a></li>
+                </ul>
+            </li>
+            <li><a><i class="fa fa-laptop"></i>Nội dung trang liên hệ<span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="{{ url('cms/widgets/contact/right_banner') }}">Banner cột phải</a></li>
                 </ul>
             </li>
             <li><a href="{{ url('cms/users') }}"><i class="fa fa-user"></i>Quản lý tài khoản</a></li>
